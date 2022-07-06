b534eef559d9e8bfa07665745e9c824d7eba0459
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-07-06 11:36:27 -0500

    added image to content

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,8 +5,11 @@ import '../index.css';
 const Content = () => {
 
     return (
-    <div className="">
-        content
+    <div className=" w-full min-h-screen h-auto flex flex-col items-center">
+        <div className=" relative text-center text-white mt-16 shadow-2xl drop-shadow-2xl">
+            <img className="min-w-full lg:max-w-[1000px] lg:max-h-[750px] grayscale-[60%] rounded-sm" alt="programing background" src="https://static.vecteezy.com/system/resources/previews/003/335/579/large_2x/desktop-source-code-and-wallpaper-by-coding-and-programming-free-photo.jpg"/>
+            <h1 className="centered_text font-['Permanent_Marker'] text-2xl lg:text-5xl">my journey so far</h1>
+        </div>
     </div>
     )
 }
