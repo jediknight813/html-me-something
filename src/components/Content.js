821dd5d76b854350135b831f959bc72c291a61d2
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-07-20 11:33:59 -0500

    changed some of the project messages

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,12 +17,12 @@ const Content = () => {
                 <h1 className="centered_text font-['Permanent_Marker'] text-2xl lg:text-5xl">my journey so far</h1>
             </div>
 
-            <p className=" w-[95%] font-sans lg:w-[1000px] text-white font-light bg-slate-700 p-10 rounded-lg shadow-2xl drop-shadow-2xl">my web development journey started over a year ago when i started the odin project, an open source bootcamp that teaches full-stack web development, here are some of the projects i have made since then.</p>
+            <p className=" w-[95%] font-sans lg:w-[1000px] text-white font-light bg-slate-700 p-10 rounded-lg shadow-2xl drop-shadow-2xl">My web development journey started over a year ago when I started the Odin project, an open source bootcamp that teaches full-stack web development. Here are some of the projects I have made since then.</p>
             
             {website_projects.websites.map(project => <ProjectDisplay Type={"website"} Project={project}/>) }
             
 
-            <p className=" w-[95%] font-light lg:w-[1000px] text-white font-sans bg-slate-700 p-10 rounded-lg shadow-2xl drop-shadow-2xl">the reason i got into programing was because i wanted to make games, i have made lots of simple games using scratch, python, unity and finally godot which is the game engine i currently use, here are some of the games i made using godot.</p>
+            <p className=" w-[95%] font-light lg:w-[1000px] text-white font-sans bg-slate-700 p-10 rounded-lg shadow-2xl drop-shadow-2xl">The reason I got into programming was because I wanted to make games. I have made lots of simple games using scratch, python, unity and finally godot which is the game engine i currently use, here are some of the games i made using godot.</p>
 
             {game_projects.games.map(game => <ProjectDisplay Type={"game"} Project={game}/>) }
 
